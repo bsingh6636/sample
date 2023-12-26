c37f0f996eed745458860afdd3c1caa0f7ed761c
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2023-12-26 19:59:45 +0545

    apilocation

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-// Replace 'YOUR_API_KEY' with the actual API key
-var apiKey = '853d7677309503d88db0457e8f66de21';
-var ipAddress = 'user_ip_address'; // You can get the user's IP address on the server-side
+// // Replace 'YOUR_API_KEY' with the actual API key
+// var apiKey = '853d7677309503d88db0457e8f66de21';
+// var ipAddress = 'user_ip_address'; // You can get the user's IP address on the server-side
 
-fetch(`https://api.ipgeolocationapi.com/geolocate/${ipAddress}?api-key=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        // Access location details like data.country_name, data.region, data.city, etc.
-    })
-    .catch(error => console.error('Error:', error));
+// fetch(`https://api.ipgeolocationapi.com/geolocate/${ipAddress}?api-key=${apiKey}`)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         // Access location details like data.country_name, data.region, data.city, etc.
+//     })
+//     .catch(error => console.error('Error:', error));
+
+
+
+function getGeolocationByIP() {
+        fetch('https://ipinfo.io/json?token=853d7677309503d88db0457e8f66de21')
+        .then((response) => response.json())
+        .then((data) => {
+            // Handle location data (country, region, city, etc.)
+          })
+        .catch((error) => {
+            // Handle error
+          });
+}
+      
