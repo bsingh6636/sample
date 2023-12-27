08e194b3bce811d91ea4f4bb48dfe9f6e71d1fc4
Brijesh Singh <bsingh6636@gmail.com>
2023-12-27 23:56:20 +0530

    slideshow

--- script.js
+++ script.js
@@ -10,8 +10,9 @@
 //     })
 //     .catch(error => console.error('Error:', error));
 
-
-
+window.onload = function() {
+  getGeolocationByIP();
+};
 function getGeolocationByIP() {
         fetch('https://ipinfo.io/json?token=853d7677309503d88db0457e8f66de21')
         .then((response) => response.json())
@@ -22,4 +23,28 @@ function getGeolocationByIP() {
             // Handle error
           });
 }
-      
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide(n){
+  showSlides(slideIndex += n);
+}
+function currentSlide(n){
+  showSlides(slideIndex = n);
+}
+     function showSlides(n){
+      let i;
+      let slides=document.getElementsByClassName("mySlides");
+      let dots =document.getElementsByClassName("dot");
+      if (n > slides.length){slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for(i = 0; i < slides.length;i++){
+        slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+     }
