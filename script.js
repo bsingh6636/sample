fc31244e55d0a867ccf1bfd54a1183cfc2aa0123
Brijesh Singh <bsingh6636@gmail.com>
2023-12-28 02:18:39 +0530

    slideshow

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function () {
   const getLocation = () => {      
       // Replace 'YOUR_API_KEY' with your actual ipinfo API key
-      const apiKey = 'ff3b255d99c518';
-
+      // const apiKey = 'ff3b255d99c518';
+      const apiKey = 'ff3b255d9kk9c518';
       fetch(`https://ipinfo.io?token=${apiKey}`)
           .then((response) => {
               if (!response.ok) {
@@ -74,38 +74,7 @@ slideshowContainer.addEventListener('mouseenter', function () {
 // Resume slideshow when not hovering
 slideshowContainer.addEventListener('mouseleave', function () {
     clearTimeout(slideshowTimeout);
-    slideshowTimeout = setTimeout(showSlides, 1300);
+    slideshowTimeout = setTimeout(showSlides, 1600);
 });
 
-let slideshowTimeout = setTimeout(showSlides, 1300);
-
-
-// let slideIndex = 0;
-// showSlides(slideIndex);
-
-// // function plusSlide(n){
-// //   showSlides(slideIndex += n);
-// // }
-
-// function plusSlide(n) {  // Corrected function name
-//   showSlides(slideIndex += n);
-// }
-// function currentSlide(n){
-//   showSlides(slideIndex = n);
-// }
-//      function showSlides(n){
-//       let i;
-//       let slides=document.getElementsByClassName("mySlides");
-//       let dots =document.getElementsByClassName("dot");
-//       if (n > slides.length){slideIndex = 1}
-//       if (n < 1) {slideIndex = slides.length}
-//       for(i = 0; i < slides.length;i++){
-//         slides[i].style.display = "none";
-//       }
-//       for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//       }
-//       slides[slideIndex-1].style.display = "block";
-//       dots[slideIndex-1].className += " active";
-//       setTimeout(showSlides, 2000);
-//      }
+let slideshowTimeout = setTimeout(showSlides, 1600);
